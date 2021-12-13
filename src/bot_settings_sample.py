5cb4211aa1976f6bbfba9f69cd1677d02343de81
GaryNLOL <dev.garynlol@gmail.com>
2021-12-13 18:34:07 -0500

    Corrected bot settings sample

--- src/bot_settings_sample.py
+++ src/bot_settings_sample.py
@@ -3,14 +3,13 @@ server_id = 90564452600745770
 
 # Role assigned by default to every member that joins.
 # Set it to `None` to not assign any role.
-default_role = 'Test'
+default_role = 'Test' # Role name or None.
 
 # Roles that can be assigned with the `role` command.
-assignable_roles = ['Test']
+assignable_roles = ['Test'] # List of role names.
 
-welcome_channel_id = '801482154680647742' # 'bienvenida-y-reglas'
-
+welcome_channel_id = 201392153884694762 # Channel id
 
 role_codes = {
-    'secret_code': 749046557054301980 # This number is the role id.
+    'secret_code': 749046557054301980 # Role id.
 }
