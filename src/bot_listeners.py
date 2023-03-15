5c3f77f4ac317bc5e0443b02e27c6284906f8e94
GaryNLOL <dev.garynlol@gmail.com>
2023-03-15 13:21:29 -0500

    Added logs on error

--- src/bot_listeners.py
+++ src/bot_listeners.py
@@ -6,12 +6,15 @@ async def on_ready(client):
     logging.getLogger('BotEvents').debug(f"Logged in successfully as {client.user}!")
 
 async def on_message(client,message):
-    content = message.content if message.content else "(Not printable)"
-    logging.getLogger('BotEvents').debug(f"Message from {message.author}: {content}.")
+    content = message.content if message.content else "(Not printable)."
+    logging.getLogger('BotEvents').debug(f"Message from {message.author}: {content}")
 
 async def on_member_join(client,member):
     if default_role:
-        server = client.get_guild(server_id)
-        role = get(server.roles,name=default_role)
-        await member.add_roles(role)
+        try:
+            server = client.get_guild(server_id)
+            role = get(server.roles,name=default_role)
+            await member.add_roles(role)
+        except:
+            logging.getLogger('BotEvents').error('Couldn\'t resolve command!')
     await client.get_channel(welcome_channel_id).send(f"{member.name} has joined!")
